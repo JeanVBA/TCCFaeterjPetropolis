90f3e041fbe556e8600903040881ee5e47a8d893
abreuje <jeanvictor999@outlook.com>
2026-02-06 20:31:58 -0300

    feat: Create project base

---
+++ Projetos/ModelosPraticos/src/Main.java
@@ -0,0 +1,37 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+        long seed = 12345;
+        long a = 1664525;
+        long c = 1013904223;
+        long mod = (long) Math.pow(2, 32);
+
+        LCG lcg = new LCG(seed, a, c, mod);
+
+        for (int i = 0; i < 10; i++) {
+            System.out.println(lcg.next());
+        }
+
+    }
+
+    public static class LCG {
+
+        private long a;
+        private long c;
+        private long mod;
+        private long xi;
+
+        public LCG(long seed, long a, long c, long mod) {
+            this.xi = seed;
+            this.a = a;
+            this.c = c;
+            this.mod = mod;
+        }
+
+        public long next() {
+            xi = (a * xi + c) % mod;
+            return xi;
+        }
+    }
+}
